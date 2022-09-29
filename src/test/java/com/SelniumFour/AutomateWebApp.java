528e64cb5f3bacbba38de169ecb4a759b8d57b0f
Ahamed <ahamed.sabid@experionglobal.com>
2022-09-29 12:36:57 +0530

    Added my project

---
+++ src/test/java/com/SelniumFour/AutomateWebApp.java
@@ -0,0 +1,112 @@
+package com.SelniumFour;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+/**
+ * Unit test for simple App.
+ */
+public class AutomateWebApp {
+	/**
+	 * Rigorous Test :-)
+	 * 
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	// @Test
+	public void shouldAnswerWithTrue() throws InterruptedException, IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to the demoqa website
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.cssSelector("input[name='q']")).sendKeys("Software");
+		Thread.sleep(3000);
+		WebElement logo = driver.findElement(By.xpath("//img[@alt='Google']"));
+		logo.click();
+		WebElement searchInput = driver.findElement(By.xpath("//input[@name='q']"));
+		// WebElement searchBtn=
+		// driver.findElement((By.xpath("//input").below(searchInput)));
+
+		File file = logo.getScreenshotAs(OutputType.FILE);
+		File destFile = new File("logoFileGoogle.png");
+		FileUtils.copyFile(file, destFile);
+		driver.quit();
+
+	}
+
+	// @Test
+	public void NewTabOpen() throws InterruptedException, IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to the demoqa website
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.get("https://www.Apple.com");
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+	// @Test
+	public void NewWindowOpen() throws InterruptedException, IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to the demoqa website
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+
+		driver.switchTo().newWindow(WindowType.WINDOW);
+		driver.get("https://www.Apple.com");
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+	@Test(description = "Verify window diamensions")
+	@Description("Verify window diamensions")
+	@Epic("EP001")
+	@Feature("Feature3: Verify window diamensions")
+	@Story("Story:User Window diamensions")
+	@Severity(SeverityLevel.NORMAL)
+	public void Location() throws InterruptedException, IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Navigate to the demoqa website
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		WebElement logo = driver.findElement(By.xpath("//img[@alt='Google']"));
+
+		System.out.println(logo.getRect().getDimension().getHeight());
+		System.out.println(logo.getRect().getDimension().getWidth());
+
+		System.out.println(logo.getRect().getX());
+		System.out.println(logo.getRect().getY());
+
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+}
---
+++ src/test/java/com/SelniumFour/RelativeLocators.java
@@ -0,0 +1,57 @@
+package com.SelniumFour;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+public class RelativeLocators {
+	
+	WebDriver driver;
+	RemoteWebDriver r;
+	
+	@BeforeTest
+	public void RelativeLocatorsBeforeTest(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		// Navigate to the demoqa website
+		driver.get("https://automationbookstore.dev/");
+		driver.manage().window().maximize();
+		
+	}
+	
+	//@Test
+	public void RelativeLocatorsTest() {
+		
+	 driver.findElement(RelativeLocator.with(By.xpath("//li")).toLeftOf(By.id("pid6"))).click();
+	}
+	@Test(description = "Verify book detail tile is clickable")
+	@Description("Verify book detail tile........")
+	@Epic("EP002")
+	@Feature("Feature3: Book store")
+	@Story("Story:User Book store")
+	@Severity(SeverityLevel. NORMAL)
+	public void RelativeLocatorsTest2() {
+		
+	 driver.findElement(RelativeLocator.with(By.xpath("//li")).above(By.id("pid6"))).click();
+	}
+	@AfterTest
+	public void close() throws InterruptedException {
+		
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
